f7a6a28bf0634d6f29befbeb5afd04a323df8b41
ralu <lucasAlejandro4@outlook.com>
2023-04-11 12:56:32 -0300

    bda v1

--- ejercicios/aerolinea/sql/aerlinea.sql
+++ ejercicios/aerolinea/sql/aerlinea.sql
@@ -1,54 +1,97 @@
-show databases;
-use bda_cfp18;
--- Borramos tablas si es que existen
-drop table if exists pasajeros;
-drop table if exists vuelos;
-drop table if exists personal;
-drop table if exists pilotos;
-drop table if exists aviones;
-drop table if exists piloto_personal;
+-- Comentario de una linea
 
--- Creamos tablas
-create table if not exists pasajeros (
-	pasaporte int primary key, 
+/* 
+	Comentario de bloque
+*/
+
+-- SHOW DATABASES; -- Muestra las bases de datos
+
+drop database if exists aerolinea; -- Borra la base de datos
+create database if not exists aerolinea;
+use aerolinea;
+
+/* 
+	drop table if exists pasajeros;
+	drop table if exists vuelos;
+	drop table if exists personal;
+	drop table if exists aviones;
+	drop table if exists pilotos;
+	drop table if exists piloto_personal;
+*/
+
+-- SHOW TABLES; -- mostrar las tablas
+
+-- Creamos tabla pasajeros
+CREATE TABLE vuelos(
+	nro int primary key,
+    hora time,
+    fecha date,
+    horaLlegada time,
+    ciudad varchar(50)
+);
+
+CREATE TABLE pasajeros(
+	pasaporte int PRIMARY KEY,
+	nro_vuelo int
+);
+
+CREATE TABLE personal(
+	nroLegajo int primary key,
+    nombre varchar(25),
+    areaAsignada enum('azafata','soporte','limpieza','piloto'),
+	nro_vuelo int
+);
+
+CREATE TABLE aviones(
+	nro int primary key,
+    modelo varchar(50),
+    fabricante varchar(50),
+    capacidad int,
+    angar varchar(12),
     nro_vuelo int
 );
 
-create table if not exists vuelos (
-	nro int primary key auto_increment,
-    horaSalida time,
-    horaLlegada time,
-    fecha date,
-    ciudad varchar(50),
-    nro_avion int
-);
-
-create table if not exists personal(
-	nroLegajo int primary key,
-    nombre varchar(25),
-    nro_vuelo int,
-    areaAsignada enum("azafata",
-						"mecanico",
-                        "soporte","limpieza","piloto")
-);
-
-create table if not exists pilotos(
+create table pilotos(
 	nroLegajo int primary key,
     nro_avion int
 );
 
-create table if not exists piloto_personal(
+create table piloto_personal(
 	id int primary key,
     nroLegajo_piloto int,
     nroLegajo_personal int
 );
 
-create table if not exists aviones(
-	nro int primary key,
-    modelo varchar(50),
-    fabricante varchar(50),
-    capacidad int,
-    angar varchar(12)
-);
+/* 
+	Relaciones entre tablas (alter table) 
+*/
 
-show tables;
+/* 
+	1) alterame la tabla pasajeros
+    2) agregame foreign key (fk) a la variable "nro_vuelo"
+    3) referenciame la fk a la variable "nro" de la tabla vuelos
+*/
+
+alter table pasajeros 
+add foreign key (nro_vuelo)
+references vuelos (nro);
+
+alter table personal 
+add foreign key (nro_vuelo)
+references vuelos (nro);
+
+alter table aviones 
+add foreign key (nro_vuelo)
+references vuelos (nro);
+
+alter table pilotos 
+add foreign key (nro_avion)
+references aviones (nro);
+
+alter table piloto_personal 
+add foreign key (nroLegajo_piloto)
+references pilotos (nroLegajo);
+
+alter table piloto_personal 
+add foreign key (nroLegajo_personal)
+references personal (nroLegajo);
